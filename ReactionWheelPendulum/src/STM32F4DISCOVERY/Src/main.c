82995df96de665492e19f1b6d7b3f322f581e93a
ssloy <dmitry.sokolov@univ-lorraine.fr>
2018-04-18 23:25:16 +0200

    adxl345 stub

--- ReactionWheelPendulum/src/STM32F4DISCOVERY/Src/main.c
+++ ReactionWheelPendulum/src/STM32F4DISCOVERY/Src/main.c
@@ -63,11 +63,17 @@
 /* Private variables ---------------------------------------------------------*/
 CAN_HandleTypeDef hcan1;
 
+I2C_HandleTypeDef hi2c1;
+
 TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim4;
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
+const uint16_t adxl_address=(0x53<<1);
+uint8_t data_rec[6];
+uint8_t chipid=0;
+
 
 volatile uint8_t system_task=0;
 
@@ -88,6 +94,7 @@ static void MX_GPIO_Init(void);
 static void MX_TIM4_Init(void);
 static void MX_CAN1_Init(void);
 static void MX_TIM3_Init(void);
+static void MX_I2C1_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
@@ -95,6 +102,30 @@ static void MX_TIM3_Init(void);
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
+void adxl_write (uint8_t reg, uint8_t value) {
+    uint8_t data[2];
+    data[0] = reg;
+    data[1] = value;
+    HAL_I2C_Master_Transmit (&hi2c1, adxl_address, data, 2, 100);
+}
+
+void adxl_read_values (uint8_t reg) {
+    HAL_I2C_Mem_Read (&hi2c1, adxl_address, reg, 1, (uint8_t *)data_rec, 6, 100);
+}
+
+void adxl_read_address (uint8_t reg) {
+    HAL_I2C_Mem_Read (&hi2c1, adxl_address, reg, 1, &chipid, 1, 100);
+}
+
+void adxl_init (void) {
+    adxl_read_address (0x00); // read the DEVID
+
+    adxl_write (0x31, 0x01);  // data_format range= +- 4g
+    adxl_write (0x2d, 0x00);  // reset all bits
+    adxl_write (0x2d, 0x08);  // power_cntl measure and wake up 8hz
+
+}
+
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 	if (htim->Instance==TIM4) {
@@ -229,10 +260,13 @@ int main(void)
   MX_CAN1_Init();
   MX_USB_DEVICE_Init();
   MX_TIM3_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
   HAL_TIM_Base_Start_IT(&htim4);
+
+  adxl_init();  // initialize adxl
 
   can_configure();
   eposReset();
@@ -256,6 +290,22 @@ int main(void)
       system_task = 0;
     }
 */
+
+    {
+    adxl_read_values (0x32);
+    int16_t x = ((data_rec[1]<<8)|data_rec[0]);
+    int16_t y = ((data_rec[3]<<8)|data_rec[2]);
+    int16_t z = ((data_rec[5]<<8)|data_rec[4]);
+
+    float xg = x * .0078;
+    float yg = y * .0078;
+    float zg = z * .0078;
+
+    char buff2[255];
+    sprintf(buff2, "%f %f %f\r\n", xg, yg, zg);
+    CDC_Transmit_FS((uint8_t *) buff2, strlen(buff2));
+
+    }
 
     if (system_task != 2) {
       char buff2[255];
@@ -412,7 +462,7 @@ static void MX_CAN1_Init(void)
 
   hcan1.Instance = CAN1;
   hcan1.Init.Prescaler = 21;
-  hcan1.Init.Mode = CAN_MODE_NORMAL;
+  hcan1.Init.Mode = CAN_MODE_LOOPBACK;
   hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan1.Init.TimeSeg1 = CAN_BS1_13TQ;
   hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
@@ -423,6 +473,26 @@ static void MX_CAN1_Init(void)
   hcan1.Init.ReceiveFifoLocked = DISABLE;
   hcan1.Init.TransmitFifoPriority = DISABLE;
   if (HAL_CAN_Init(&hcan1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* I2C1 init function */
+static void MX_I2C1_Init(void)
+{
+
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
   }
--- ReactionWheelPendulum/src/STM32F4DISCOVERY/Src/stm32f4xx_hal_msp.c
+++ ReactionWheelPendulum/src/STM32F4DISCOVERY/Src/stm32f4xx_hal_msp.c
@@ -147,6 +147,60 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef* hcan)
 
 }
 
+void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+  if(hi2c->Instance==I2C1)
+  {
+  /* USER CODE BEGIN I2C1_MspInit 0 */
+
+  /* USER CODE END I2C1_MspInit 0 */
+  
+    /**I2C1 GPIO Configuration    
+    PB6     ------> I2C1_SCL
+    PB7     ------> I2C1_SDA 
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C1_CLK_ENABLE();
+  /* USER CODE BEGIN I2C1_MspInit 1 */
+
+  /* USER CODE END I2C1_MspInit 1 */
+  }
+
+}
+
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
+{
+
+  if(hi2c->Instance==I2C1)
+  {
+  /* USER CODE BEGIN I2C1_MspDeInit 0 */
+
+  /* USER CODE END I2C1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_I2C1_CLK_DISABLE();
+  
+    /**I2C1 GPIO Configuration    
+    PB6     ------> I2C1_SCL
+    PB7     ------> I2C1_SDA 
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_6|GPIO_PIN_7);
+
+  /* USER CODE BEGIN I2C1_MspDeInit 1 */
+
+  /* USER CODE END I2C1_MspDeInit 1 */
+  }
+
+}
+
 void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef* htim_encoder)
 {
 
