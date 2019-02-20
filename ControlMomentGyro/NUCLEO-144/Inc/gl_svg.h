78e0a122cc43477e6bc9f510814e33a031ec9619
Dmitry V. Sokolov <ha@haqr.eu>
2019-02-20 21:12:51 +0100

    GL-SVG 109 modifications

--- ControlMomentGyro/NUCLEO-144/Inc/gl_svg.h
+++ ControlMomentGyro/NUCLEO-144/Inc/gl_svg.h
@@ -2,8 +2,21 @@
 #define GL_SVG_H
 
 #include "stm32f7xx_hal.h"
+#include "stdbool.h"
 
-typedef struct svgStruct{
+#define FRAME_LENGTH 200
+#define BINS_FRAME_LENGTH 58
+#define GYRO_FRAME_SIZE 120
+
+#define ORIENTATION_PACKET1 0x70
+#define ORIENTATION_PACKET2 0xAF
+#define STATUS_PACKET 0xE6
+
+#define BINS_PACKET 0x70
+#define EXTRA_PACKET 0x72
+#define NUMBER_OF_AV_PACKS 2
+
+typedef struct orientationPacket1Struct{
   uint32_t status;
 
   float ax;
@@ -22,28 +35,36 @@ typedef struct svgStruct{
   int32_t longtitude;
   float height;
 
-}__attribute__((packed)) svgStruct;
+}__attribute__((packed)) orientationPacket1Struct;
 
-svgStruct svgData;
+typedef struct orientationPacket2Struct{
+  float gx;
+  float gy;
+  float gz;
+  
+  float north_course;
+  float roll;
+  float pitch;
+  float stab_course;
+}__attribute__((packed)) orientationPacket2Struct;
 
-void setSVGUART(UART_HandleTypeDef * handler);
-void add_CRC(uint16_t *fcs, uint8_t c);
-void processSVG();
-float getSVGGx();
-float getSVGGy();
-float getSVGGz();
-float getSVGYaw();
-float getSVGRoll();
-float getSVGPitch();
-void dropDataFlag();
+extern orientationPacket1Struct svg_data1;
+extern orientationPacket2Struct svg_data2;
 
-uint32_t getSVGStatus();
-uint8_t isReady();
-uint8_t isNewFrame();
-uint8_t isValidPacket(uint8_t pack_id);
+void GLVG_init(UART_HandleTypeDef * uart, float * coordinates);
+void GLVG_reinitCoordinates(float lat, float lon, float h);
 
-void resetNewFrame();
-void resetError();
-void initSVG();
-float getDistanceFromLatLon(float lat1,float lon1,float lat2,float lon2);
+bool GLVG_isINSValid();
+bool GLVG_isMoving();
+bool GLVG_isReady();
+bool GLVG_isFirstPacketReceived();
+
+uint16_t GLVG_getFPS();
+float GLVG_getRoll();
+float GLVG_getYaw();
+float GLVG_getPitch();
+float GLVG_getGx();
+float GLVG_getGy();
+float GLVG_getGz();
+
 #endif
--- ControlMomentGyro/NUCLEO-144/Src/gl_svg.c
+++ ControlMomentGyro/NUCLEO-144/Src/gl_svg.c
@@ -1,271 +1,314 @@
-#include "gl_svg.h"
+#include "../../libs/inc/gl_svg.h"
 
-#define FRAME_LENGTH 200
-#define BINS_FRAME_LENGTH 58
-#define EXTRA_FRAME_LENGTH 54
-#define PRINT_BUFFER_SIZE 100
-#define COMMANDS_BUFFER_LENGTH 15
-#define GYRO_FRAME_SIZE 500
+#include "string.h"
+#include "math.h"
 
-#define BINS_PACKET 0x70
-#define EXTRA_PACKET 0x72
-#define NUMBER_OF_AV_PACKS 2
+#ifdef GLVG_485
+#define GLVG_PACKET_SHIFT 	4
+#else
+#define GLVG_PACKET_SHIFT 	0
+#endif
 
-#define GRAVITY_ACCELERATION 9.8065
+#define TX_BUFFER_SIZE		30
 
-uint8_t availablePackets[NUMBER_OF_AV_PACKS] = {BINS_PACKET, EXTRA_PACKET};
+#define GL_RESET 			1
+#define GL_REINIT			2
+#define GL_REINIT_WAITING	3
+#define GL_NONE				0
 
-char PrintBuffer[PRINT_BUFFER_SIZE];
-uint8_t frame[FRAME_LENGTH], length, buffer = 0;
-uint8_t commands_buffer[COMMANDS_BUFFER_LENGTH];
-uint8_t gyro_rx_buffer[GYRO_FRAME_SIZE];
+static uint8_t GLVG_frame[GYRO_FRAME_SIZE];
+static uint8_t rxBuffer[GYRO_FRAME_SIZE];
+static uint8_t txBuffer[TX_BUFFER_SIZE];
 
-uint8_t data_ready = 0;
+orientationPacket1Struct svg_data1;
+orientationPacket2Struct svg_data2;
+static uint32_t GLVG_statusWord;
 
-float ax, ay, az;
-float gx, gy, gz;
-float yaw, pitch, roll;
-float latitude1, latitude2, longtitude1, longtitude2;
+static bool packetStarted = false;
 
-uint32_t status;
-uint8_t is_new_frame = 0, is_ready = 0, is_processing = 0;
-uint8_t packet_started;
-uint8_t fault_state = 0;
+static uint8_t packetType = 0;
+static uint8_t packetLength = 0;
 
-uint8_t gyroRxIdx = 0, gyroTxIdx = 0;
-uint8_t packet_counter = 0;
-uint8_t packet_type;
-uint16_t packet_length;
-uint16_t whole_n_packs = 0;
-uint16_t valid_packs = 0;
+uint8_t type = 0;
+uint8_t size = 0;
 
-uint32_t last_time;
-UART_HandleTypeDef* gyro_uart;
+static uint8_t rxBufferIdx = 0;
+static uint8_t txBufferIdx = 0;
 
-uint16_t ccitt_crc16_table[256] = {
-0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
-0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
-0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
-0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
-0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
-0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac,
-0xd58d, 0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695,
-0x46b4, 0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d,
-0xc7bc, 0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802,
-0x3823, 0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a,
-0xb92b, 0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33,
-0x2a12, 0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b,
-0xab1a, 0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60,
-0x1c41, 0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68,
-0x9d49, 0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51,
-0x0e70, 0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59,
-0x8f78, 0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e,
-0xe16f, 0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046,
-0x6067, 0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f,
-0xf35e, 0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277,
-0x7256, 0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c,
-0xc50d, 0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424,
-0x4405, 0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d,
-0xd73c, 0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615,
-0x5634, 0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a,
-0xa9ab, 0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882,
-0x28a3, 0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb,
-0xbb9a, 0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3,
-0x3a92, 0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8,
-0x8dc9, 0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0,
-0x0cc1, 0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9,
-0x9ff8, 0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1,
-0x1ef0
-};
+static uint8_t commandSize = 0;
+static bool commandSent = true;
+static uint32_t commandSentTime;
 
-void add_CRC(uint16_t * fcs, uint8_t c)
-{
-	*fcs = ccitt_crc16_table[(*fcs >> 8 ^ c) & 0xff] ^ (*fcs << 8);
+static uint16_t validPacks, wholePacks;
+
+static uint32_t lastPacketTime;
+static uint8_t GLVG_command = GL_NONE;
+static uint8_t GLVG_nextCommand = GL_NONE;
+static bool GLVG_firstPacketReceived = false;
+static bool GLVG_coordinatesInitialized = false;
+
+static uint32_t GLVG_latitude = 0;
+static uint32_t GLVG_longitude = 0;
+static uint32_t GLVG_height = 0;
+static uint32_t GLVG_installCommandId = 4359;
+
+UART_HandleTypeDef * GLVG_uart;
+
+void GLVG_init(UART_HandleTypeDef * uart, float * coordinates){
+	GLVG_uart = uart;
+	GLVG_latitude = (uint32_t)(coordinates[0] * M_PI / 180 * 100000000);
+	GLVG_longitude = (uint32_t)(coordinates[1] * M_PI / 180 * 100000000);
+	GLVG_height = (uint32_t)(coordinates[2] * 100000);
+
+	__HAL_UART_ENABLE_IT(GLVG_uart, UART_IT_RXNE);
+  	
 }
 
-void setSVGUART(UART_HandleTypeDef* handler){
-	gyro_uart = handler;
-	__HAL_UART_ENABLE_IT(gyro_uart, UART_IT_RXNE);
+static float GLVG_calculateDistance(float lat1, float lon1, float lat2, float lon2){
+	float R = 6371e3;
+	float f1 = lat1;
+	float f2 = lat2;
+	float df = (lat2 - lat1);
+	float dy = (lon2 - lon1);
+
+	float a = sin(df / 2.0) * sin(df / 2.0) +
+			  cos(f1) * cos(f2) * sin(dy / 2.0)*
+			  sin(dy / 2.0);
+	float c = 2 * atan2(sqrt(a), sqrt(1-a));
+
+	return R * c;
+
 }
 
-void processSVG() {
-	whole_n_packs++;
-	uint16_t wData;
+static int GLVG_findPacketIndex(uint8_t * buffer, uint8_t buffer_length){
+	uint8_t i = 0;
+
+	for(; i < buffer_length - 2 && ((buffer[i] != 0xAA) | (buffer[i+1] != 0xAA)
+									| (buffer[i + 2] != 0x37)); ++i);
+
+	if(i + BINS_FRAME_LENGTH < buffer_length) return i;
+
+	return -1;
+}
+
+static uint16_t GLVG_calcCRC(uint8_t * buffer, uint8_t size){
 	uint16_t wCrc = 0x00;
 	uint8_t i, j;
-	for (i = 3; i < packet_length - 2; i++) {
-		wCrc ^= frame[i] << 8;
+
+	for (i = 0; i < size; i++) {
+		wCrc ^= buffer[i] << 8;
 		for (j = 0; j < 8; j++)
 			wCrc = wCrc & 0x8000 ? (wCrc << 1) ^ 0x1021 : wCrc << 1;
 	}
+
 	wCrc = wCrc & 0xffff;
-	if (wCrc
-			!= (frame[packet_length - 2]) + ((frame[packet_length - 1]) << 8)) {
+	return wCrc;
+}
 
-	} else {
-		valid_packs++;
-		if (packet_type == BINS_PACKET) {
-			memcpy(&(svgData), &frame[8], 52);
+void GLVG_processCommands(){
+	uint8_t status = (svg_data1.status >> 28) & 0b11;
 
-			if (svgData.status & 0x01) {
-				// gyroscope fault state handler
-				fault_state = 1;
-			} else {
-				fault_state = 0;
+	float distance = 0;
+	if(GLVG_isFirstPacketReceived() && !GLVG_coordinatesInitialized){
+		distance = GLVG_calculateDistance(svg_data1.latitude / 100000000.0f, svg_data1.longtitude / 100000000.0f,
+						 GLVG_latitude / 100000000.0f, GLVG_longitude / 100000000.0f);
+	}
+
+	switch(GLVG_command){
+		case GL_REINIT:
+			if((status == 0b01 || status == 0b11)
+						&& commandSent){
+				txBuffer[0] = 0xAA;
+				txBuffer[1] = 0xAA;
+				txBuffer[2] = 23;
+				txBuffer[3] = 0x45;
+				memcpy(&txBuffer[4], &GLVG_latitude, 4);
+				memcpy(&txBuffer[8], &GLVG_longitude, 4);
+				memcpy(&txBuffer[12], &GLVG_height, 4);
+				memcpy(&txBuffer[20], &GLVG_installCommandId, 4);
+
+				uint16_t crc = GLVG_calcCRC(txBuffer + 3, 21);
+				txBuffer[24] = crc & 0xFF;
+				txBuffer[25] = (crc >> 8) & 0xFF;
+
+				commandSize	= 26;
+				commandSent	= false;
+				GLVG_command = GL_REINIT_WAITING;
+
+				__HAL_UART_DISABLE_IT(GLVG_uart, UART_IT_RXNE);
+				__HAL_UART_ENABLE_IT(GLVG_uart, UART_IT_TXE);
+
+			}else if(status == 0b10){
+				GLVG_command = GL_RESET;
 			}
+			break;
+		case GL_RESET:
+			GLVG_reset();
+			GLVG_command = GL_REINIT;
+			break;
+		case GL_REINIT_WAITING:
+			if(commandSent){
+				if(distance > 5000 && commandSentTime > 2000){
+					GLVG_command = GL_REINIT;
+				}else{
+					GLVG_command = GL_NONE;
+				}
+			}
+			break;
+		case GL_NONE:
+			if(GLVG_isFirstPacketReceived()){
+				if(distance > 5000){
+					GLVG_command = GL_REINIT;
+				}else{
+					GLVG_coordinatesInitialized = true;
+				}
+			}
+			break;
+	}
+}
 
+void GLVG_reinitCoordinates(float lat, float lon, float h){
+	GLVG_latitude = (uint32_t)(lat * M_PI / 180 * 100000000);
+	GLVG_longitude = (uint32_t)(lon * M_PI / 180 * 100000000);
+	GLVG_height = (uint32_t)(h * 100000);
+
+	GLVG_command = GL_REINIT;
+	GLVG_coordinatesInitialized = false;
+}
+
+void GLVG_reset(){
+	txBuffer[0] = 0xAA;
+	txBuffer[1] = 0xAA;
+	txBuffer[2] = 0x03;
+	txBuffer[3] = 0x88;
+	txBuffer[4] = 0x80;
+	txBuffer[5] = 0x10;
+
+	commandSize = 6;
+	commandSent = false;
+
+	__HAL_UART_DISABLE_IT(GLVG_uart, UART_IT_RXNE);
+	__HAL_UART_ENABLE_IT(GLVG_uart, UART_IT_TXE);
+
+}
+
+static void GLVG_parsePacket(){
+	if(HAL_GetTick() - lastPacketTime > 1000){
+		wholePacks = validPacks;
+		validPacks = 0;
+		lastPacketTime = HAL_GetTick();
+	}
+
+	uint16_t wCRC = GLVG_frame[size - 2] | (GLVG_frame[size - 1] << 8);
+	if (GLVG_calcCRC(GLVG_frame + 3, size - 5) == wCRC) {
+		// svgStruct svgData;
+		GLVG_firstPacketReceived = true;
+
+		validPacks++;
+		if (type == ORIENTATION_PACKET1) {
+			memcpy(&svg_data1, &GLVG_frame[4 + GLVG_PACKET_SHIFT], size - 6);
 		}
-
-		if (packet_type == EXTRA_PACKET) {
-			float dt = (float) (HAL_GetTick() - last_time) / 1000;
-			last_time = HAL_GetTick();
-
+		if (type == STATUS_PACKET) {
+			memcpy(&GLVG_statusWord, &GLVG_frame[4 + GLVG_PACKET_SHIFT], 4);
 		}
 
 	}
 
-	is_ready = 0;
 }
 
+bool GLVG_isFirstPacketReceived(){
+	return GLVG_firstPacketReceived;
+}
 
+bool GLVG_isINSValid(){
+	return (svg_data1.status & 0x01) == 0 ? true : false;
+}
 
-uint8_t svgIRQHandler(UART_HandleTypeDef* huart) {
-	if (gyro_uart->Instance == huart->Instance) {
-		if ((__HAL_UART_GET_FLAG(huart, UART_FLAG_RXNE) != RESET)
-				&& (__HAL_UART_GET_IT_SOURCE(huart, UART_IT_RXNE) != RESET)) {
-			if (gyroRxIdx < GYRO_FRAME_SIZE) {
-				gyro_rx_buffer[gyroRxIdx] = huart->Instance->RDR;
-				if (gyroRxIdx > 2
-					 	&&	gyro_rx_buffer[gyroRxIdx-3] == 0xAA
-						&& gyro_rx_buffer[gyroRxIdx-2] == 0xAA
+bool GLVG_isMoving(){
+	return ((svg_data1.status >> 6) & 0x01) == 0 ? true : false;
+}
 
-						&& !packet_started){
-					packet_type = gyro_rx_buffer[gyroRxIdx ];
-					packet_length = gyro_rx_buffer[gyroRxIdx-1]+3 ;
-					packet_started = 1;
-					packet_counter = 3;
-				}
-				gyroRxIdx++;
+bool GLVG_isReady(){
+	return ((svg_data1.status >> 15) & 0x01);
+}
 
-				if(packet_started){
-					packet_counter++;
-				}
+uint16_t GLVG_getFPS(){
+	return wholePacks;
+}
 
-				if((packet_counter == packet_length) && packet_started ){
-					//if(!is_ready){
-						memset(&frame,0,FRAME_LENGTH);
-						memcpy(&frame, &gyro_rx_buffer[gyroRxIdx - packet_length], packet_length);
-						memset(&gyro_rx_buffer,0,GYRO_FRAME_SIZE);
-						processSVG();
-						is_ready = 1;
+float GLVG_getRoll(){
+	return svg_data1.roll;
+}
 
-					//}
-					packet_started = 0;
-					packet_counter = 0;
-					gyroRxIdx = 0;
-				}
+float GLVG_getYaw(){
+	return svg_data1.yaw;
+}
 
+float GLVG_getPitch(){
+	return svg_data1.pitch;
+}
 
-			}else{
-				gyroRxIdx = 0;
+float GLVG_getGx(){
+	return svg_data1.gx;
+}
+
+float GLVG_getGy(){
+	return svg_data1.gy;
+}
+
+float GLVG_getGz(){
+	return svg_data1.gz;
+}
+
+void GLVG_UART_IRQ_Handler(UART_HandleTypeDef * huart) {
+    if(huart->Instance == GLVG_uart->Instance){
+		if((__HAL_UART_GET_FLAG(huart, UART_FLAG_RXNE) != RESET)
+			  && (__HAL_UART_GET_IT_SOURCE(huart, UART_IT_RXNE) != RESET)) {
+			if(rxBufferIdx >= GYRO_FRAME_SIZE) rxBufferIdx = 0;
+
+			rxBuffer[rxBufferIdx++] =  huart->Instance->RDR;
+
+			if(rxBufferIdx > 3 && rxBuffer[rxBufferIdx - 4] == 0xAA && rxBuffer[rxBufferIdx - 3] == 0xAA){
+				rxBuffer[0] = rxBuffer[rxBufferIdx - 4];
+				rxBuffer[1] = rxBuffer[rxBufferIdx - 3];
+				packetLength = rxBuffer[2] = rxBuffer[rxBufferIdx - 2];
+				packetType = rxBuffer[3] = rxBuffer[rxBufferIdx - 1];
+				rxBufferIdx = 4;
+				packetStarted = true;
+
 			}
-			__HAL_UART_FLUSH_DRREGISTER(huart);
-			return 1;
+
+			if(packetStarted && rxBufferIdx == packetLength + 3){
+				rxBufferIdx = 0;
+				packetStarted = 0;
+
+				size = packetLength + 3;
+				type = packetType;
+				memcpy(&GLVG_frame, &rxBuffer, size);
+				GLVG_parsePacket();
+			}
+
 		}
 
-		if((__HAL_UART_GET_FLAG(huart, UART_FLAG_TXE) != RESET) &&(__HAL_UART_GET_IT_SOURCE(huart, UART_IT_TXE) != RESET)) {
-			huart->Instance->RDR = commands_buffer[gyroTxIdx++];
+		if((__HAL_UART_GET_FLAG(huart, UART_FLAG_TXE) != RESET)
+						&& (__HAL_UART_GET_IT_SOURCE(huart, UART_IT_TXE) != RESET)){
 
-			if(gyroTxIdx == strlen(commands_buffer)){
-				gyroTxIdx = 0;
+			if(txBufferIdx < commandSize){
+				huart->Instance->TDR = txBuffer[txBufferIdx++];
+			} else{
+				txBufferIdx = 0;
+
+				while(__HAL_UART_GET_FLAG(huart, UART_FLAG_TC) == RESET);
 				__HAL_UART_DISABLE_IT(huart, UART_IT_TXE);
+				__HAL_UART_ENABLE_IT(huart, UART_IT_RXNE);
+
+				memset(&txBuffer,0,TX_BUFFER_SIZE);
+				commandSent = true;
+				commandSentTime = HAL_GetTick();
+
 			}
-
-
-			__HAL_UART_FLUSH_DRREGISTER(huart);
-			return 1;
 		}
+		__HAL_UART_FLUSH_DRREGISTER(huart);
 
-	}
-	__HAL_UART_FLUSH_DRREGISTER(huart);
-	return 0;
-}
-
-uint8_t isValidPacket(uint8_t pack_id){
-	uint8_t i;
-	for(i = 0; i < NUMBER_OF_AV_PACKS; i++)
-		if(availablePackets[i] == pack_id) return 1;
-
-	return 0;
-}
-
-float getDistanceFromLatLon(float lat1,float lon1,float lat2,float lon2) {
-  int R = 6371;
-  float dLat = (lat2-lat1);
-  float dLon = (lon2-lon1);
-  float a =
-    sin(dLat/2) * sin(dLat/2) +
-    cos((lat1)) * cos((lat2)) *
-    sin(dLon/2) * sin(dLon/2);
-  float c = 2 * atan2(sqrt(a), sqrt(1-a));
-  float d = R * c;
-  return d*1000;
-}
-
-
-void resetError(){
-	memset(commands_buffer, 0, COMMANDS_BUFFER_LENGTH);
-	commands_buffer[0] = 0xAA;
-	commands_buffer[1] = 0xAA;
-	commands_buffer[2] = 0x07;
-	commands_buffer[3] = 0x88;
-	commands_buffer[4] = 0xE1;
-	commands_buffer[5] = 0xCD;
-	commands_buffer[6] = 0x28;
-	commands_buffer[7] = 0x64;
-	commands_buffer[8] = 0x68;
-	commands_buffer[9] = 0x15;
-
-	gyroTxIdx = 0;
-	__HAL_UART_ENABLE_IT(gyro_uart, UART_IT_TXE);
-}
-
-float getSVGGx(){
-	return svgData.gx;
-}
-
-float getSVGGy(){
-	return svgData.gy;
-}
-
-float getSVGGz(){
-	return svgData.gz;
-}
-
-float getSVGYaw(){
-	return svgData.yaw;
-}
-
-float getSVGRoll(){
-	return svgData.roll;
-}
-
-float getSVGPitch(){
-	return svgData.pitch;
-}
-
-uint8_t isInFaultState(){
-	return fault_state;
-}
-
-uint8_t isReady(){
-	return is_ready;
-}
-
-void dropDataFlag(){
-	data_ready = 0;
-}
-
-uint32_t getSVGStatus(){
-	return status;
+    }
 }
